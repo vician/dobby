e57a7016d9ab4294e0927d7f1a7a7e800677c6b2
Martin Vician <info@vician.cz>
2016-08-25 21:46:38 +0200

    added youtube

---
+++ reactions/youtube.py
@@ -0,0 +1,51 @@
+from reactions.reaction import Reaction
+
+import subprocess
+import threading
+import pafy
+import os
+
+class Youtube(Reaction):
+
+    help = "Simple playing audio from youtube."
+    aliasses = [ "yt" ]
+
+    mplayer = "mplayer"
+    yturl = ""
+    playThread = None
+
+    def do(self,message):
+        if message == "stop":
+            self.stop_youtube
+        self.stop_youtube()
+        self.play_youtube(message)
+        pass
+
+    def ytThread(self):
+        if len(self.yturl) > 0:
+            #subprocess.call(["mplayer", self.yturl])
+            args = ["mplayer", self.yturl]
+            subprocess.Popen(args)
+
+
+    def play_youtube(self,url):
+        if len(url) > 0:
+            v = pafy.new(url)
+            bestaudio = v.getbestaudio()
+            self.yturl = bestaudio.url
+            if threading.active_count() <= 1:
+                self.playThread = threading.Thread(target=self.ytThread)
+                self.playThread.start()
+                return "playing: {}".format(v.title)
+            else:
+                return "ERROR: threading.active_count() == {}".format(threading.active_count())
+
+    def stop_youtube(self):
+        if self.playThread is not None:
+            subprocess.call(["killall", self.mplayer])
+            self.playThread.join()
+            self.playThread = None
+            return "YouTube player was stopped"
+        else:
+            return "YouTube player was not running. Nothing to stop."
+
