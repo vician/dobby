9b61ef109d82603be3c62ff32ee4db6dab665767
Martin Vician <martin.vician@nic.cz>
2016-08-26 14:11:24 +0200

    tts can be disabled

--- tts/gtts.py
+++ tts/gtts.py
@@ -11,13 +11,15 @@ class Gtts(Tts):
     def init(self):
         self.mplayer = Mplayer()
         self.ini.set("lang","en")
+        self.ini.set("disabled","0")
 
     def say(self,message):
-        if message != None and len(message) > 0:
-            audio_file = tempfile.mktemp()
-            tts = gTTS(text=message, lang=self.ini.get("lang"))
-            tts.save(audio_file)
-            self.mplayer.play(audio_file)
+        if self.ini.get("disabled") != "1":
+            if message is str and len(message) > 0:
+                audio_file = tempfile.mktemp()
+                tts = gTTS(text=message, lang=self.ini.get("lang"))
+                tts.save(audio_file)
+                self.mplayer.play(audio_file)
 
     def do(self,message=None):
         if message == "stop" or message == "quiet":
