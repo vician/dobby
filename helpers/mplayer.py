a6bd95d1a9320b0e4a2a9bb669093c0f25e8765a
Martin Vician <martin.vician@nic.cz>
2016-08-26 10:07:02 +0200

    fixed playing, stderr to null

--- helpers/mplayer.py
+++ helpers/mplayer.py
@@ -4,17 +4,19 @@ import threading
 class Mplayer():
 
     playThread = None
+    audio_file = ""
 
     def __init__(self):
         pass
 
-    def thread(self,audio_file):
-        if len(audio_file) > 0:
-            args = [ "mplayer", "-really-quiet", audio_file]
-            subprocess.Popen(args)
+    def thread(self):
+        if len(self.audio_file) > 0:
+            args = [ "mplayer", "-really-quiet", self.audio_file]
+            subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
 
     def play(self,audio_file):
         if len(audio_file) > 0:
+            self.audio_file = audio_file
             if threading.active_count() <= 1:
                 self.playThread = threading.Thread(target=self.thread)
                 self.playThread.start()
