d83bdb76206b85570f3d58f0052f37ce512d3f11
Martin Vician <martin.vician@nic.cz>
2016-08-26 16:11:34 +0200

    Example with attributes

--- reactions/example.py
+++ reactions/example.py
@@ -5,10 +5,13 @@ class Example(Reaction): # Change name of this class!
 
     help = "Here write information about this module!"
     aliasses = [ "example" ]
+    attributes = [] # Specify your attributes
 
     def init(self):
-        # Your init function. Do not use __init__!!!
+        # Your init function. Do not use __init__!!!a
+        self.ini.set("foo","bar") # Init your attributes
         pass
 
     def do(self,message):
+        something = self.ini.get("foor") # Use your attribute
         return "Whatevere you want!" # Change to your behaviour
