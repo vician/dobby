6caf37843b5ff6dca7e5107fe8313f66a48261b0
Martin Vician <martin.vician@nic.cz>
2016-08-26 09:22:52 +0200

    using tempfile

--- tts/gtts.py
+++ tts/gtts.py
@@ -2,11 +2,12 @@ from tts.tts import Tts
 
 from gtts import gTTS
 import subprocess
+import tempfile
 
 class Gtts(Tts):
 
     def say(self,message):
-        audio_file = "hello.mp3"
+        audio_file = tempfile.mktemp()
         tts = gTTS(text=message, lang="en")
         tts.save(audio_file)
         return_code = subprocess.call(["mplayer", audio_file])
