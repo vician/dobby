ded8b4b055f2028c8b4e71f1580ef8cc00aead21
Martin Vician <martin.vician@nic.cz>
2016-08-26 14:14:24 +0200

    prepared helpers for language codes

---
+++ helpers/language.py
@@ -0,0 +1,19 @@
+class Language()
+
+    def to_short(self,long_name):
+        long_name = long_name.lower()
+        if long_name == "czech":
+            return "cs"
+        if long_name == "english":
+            return "en"
+
+        return long_name
+
+    def to_long(self,short_name):
+        short_name = short_name.lower()
+        if short_name == "cs":
+            return "Czech"
+        if short_name == "en":
+            return "English"
+
+        return short_name
